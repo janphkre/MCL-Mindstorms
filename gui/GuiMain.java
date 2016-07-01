0b8fd55f82aa5aa205bafadc0c9c5efedafc6fa4
janphkre <janphkre@gmx.de>
2016-07-01 23:00:31 +0200

    Bugfixing.

--- gui/GuiMain.java
+++ gui/GuiMain.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.io.File;
 
+import aima.core.robotics.IMclRobot;
 import aima.core.robotics.impl.MonteCarloLocalization;
 import aima.core.robotics.impl.datatypes.Angle;
 import aima.core.robotics.impl.datatypes.RangeReading;
@@ -9,8 +10,8 @@ import aima.core.robotics.impl.map.MclCartesianPlot2D;
 import aima.core.util.math.geom.SVGGroupParser;
 import aima.gui.applications.robotics.MonteCarloLocalizationApp;
 import aima.gui.applications.robotics.components.AnglePanel;
+import aima.gui.applications.robotics.components.IRobotGui;
 import aima.gui.applications.robotics.components.Settings;
-import aima.gui.applications.robotics.util.SimpleSettingsListener;
 import bot.Connector;
 import localization.NXTMove;
 import localization.NXTPosition;
@@ -18,18 +19,22 @@ import localization.NXTPositionFactory;
 import localization.NXTRangeReading;
 import localization.NXTRangeReadingFactory;
 
-public class GuiMain {
+public class GuiMain extends MonteCarloLocalizationApp<NXTPosition, NXTMove, NXTRangeReading> {
 	
-	private static final File DEFAULT_SETTINGS_FILE = new File("");//TODO
+	public GuiMain(MonteCarloLocalization<NXTPosition, Angle, NXTMove, RangeReading> mcl, MclCartesianPlot2D<NXTPosition, NXTMove, RangeReading> map, IMclRobot<Angle, NXTMove, RangeReading> robot, IRobotGui robotGui, Settings settingsGui) {
+		super(mcl, map, robot, robotGui, settingsGui);
+	}
+
+	protected static final File DEFAULT_SETTINGS_FILE = new File("");//TODO
 	
 	public static void main(String[] args) {
 		Settings settingsGui = MonteCarloLocalizationApp.buildSettings(args.length > 0 ? new File(args[0]) : DEFAULT_SETTINGS_FILE);
 		NXTSettingsListener settingsListener = new NXTSettingsListener(settingsGui);
 		settingsListener.createSettings();
-		settingsGui.registerSetting(SimpleSettingsListener.PARTICLE_COUNT_KEY, "Particle count", "1000");
+		settingsGui.registerSetting(PARTICLE_COUNT_KEY, "Particle count", "1000");
 		
 		final double sensorRange = Double.parseDouble(settingsGui.getSetting(NXTSettingsListener.SENSOR_RANGE_KEY));
-		final int particleCount = Integer.parseInt(settingsGui.getSetting(SimpleSettingsListener.PARTICLE_COUNT_KEY));
+		final int particleCount = Integer.parseInt(settingsGui.getSetting(PARTICLE_COUNT_KEY));
 		final double minWeight = Double.parseDouble(settingsGui.getSetting(NXTSettingsListener.MIN_WEIGHT_KEY));
 		final double maxDistance = Double.parseDouble(settingsGui.getSetting(NXTSettingsListener.MAX_DISTANCE_KEY));
 		AnglePanel angles = new AnglePanel();
@@ -38,7 +43,7 @@ public class GuiMain {
 		MclCartesianPlot2D<NXTPosition, NXTMove, RangeReading> map = new MclCartesianPlot2D<NXTPosition,NXTMove,RangeReading>(new SVGGroupParser(),new SVGGroupParser(),new NXTPositionFactory(),new NXTRangeReadingFactory(),sensorRange);
 		Connector robot = new Connector(angles.getSetting());
 		MonteCarloLocalization<NXTPosition,Angle,NXTMove,RangeReading> mcl = new MonteCarloLocalization<NXTPosition, Angle, NXTMove, RangeReading>(map, robot, particleCount, minWeight, maxDistance);
-		MonteCarloLocalizationApp<NXTPosition, NXTMove, NXTRangeReading> app = new MonteCarloLocalizationApp<NXTPosition,NXTMove,NXTRangeReading>(mcl, map, robot, new NXTRobotGui(robot), settingsGui);
+		GuiMain app = new GuiMain(mcl, map, robot, new NXTRobotGui(robot), settingsGui);
 		
 		angles.setChangeListener(robot);
 		settingsListener.setMap(map);
